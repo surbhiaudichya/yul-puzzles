25836c87ba5dfc2c48e863c0fb3f08810a003554
Surbhi Audichya <audichyasurbhi@gmail.com>
2025-04-29 13:15:15 +0530

    feat: add RevertWithSelector yul puzzels solution

--- src/RevertWithSelector.sol
+++ src/RevertWithSelector.sol
@@ -12,6 +12,8 @@ contract RevertWithSelector {
             // `revert RevertData()`
             // but in assembly
             // hint: https://www.rareskills.io/post/assembly-revert
+            mstore(0x00, 0xa3b7e096) // bytes4 selector = bytes4(abi.encodeWithSignature("RevertData()"));
+            revert(0x1c, 0x04) //  function selector is at the 28th byte (0x1c in hex)
         }
     }
 }
