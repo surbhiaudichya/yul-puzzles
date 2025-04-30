0b3bc5d44d8aafee9617cb232f73317609825a1e
Surbhi Audichya <audichyasurbhi@gmail.com>
2025-04-30 10:50:50 +0530

    feat: add RevertWithSelectorPlusArgs yul puzzels solution

--- src/RevertWithSelectorPlusArgs.sol
+++ src/RevertWithSelectorPlusArgs.sol
@@ -10,6 +10,9 @@ contract RevertWithSelectorPlusArgs {
             // revert custom error with x parameter
             // Hint: concatenate selector and x by storing them
             // adjacent to each other in memory
+            mstore(0x00, 0xae41228700000000000000000000000000000000000000000000000000000000)
+            mstore(0x04, x)
+            revert(0x00, 0x24)
         }
     }
 }
