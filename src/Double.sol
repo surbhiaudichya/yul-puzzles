a9ed5eec6b97034831cb12a1d0285e7843b61fc2
Surbhi Audichya <audichyasurbhi@gmail.com>
2025-05-01 19:25:53 +0530

    feat: add Double yul puzzels solution

--- src/Double.sol
+++ src/Double.sol
@@ -2,17 +2,19 @@
 pragma solidity ^0.8.13;
 
 contract Double {
+    function main(uint256 x) external pure returns (uint256) {
+        assembly {
+            // your code here
+            // return 2 * x using assembly
+            // assume x will always be less
+            // than half type(uint256).max
+            // so it won't overflow
+            // hint: x can be directly accessed in assembly
 
-  function main(uint256 x) external pure returns (uint256) {
-      assembly {
-          // your code here
-          // return 2 * x using assembly
-          // assume x will always be less
-          // than half type(uint256).max
-          // so it won't overflow
-          // hint: x can be directly accessed in assembly
-
-          // see here for how to multiply in YUL: https://docs.soliditylang.org/en/latest/yul.html#evm-dialect
-      }
-  }
+            // see here for how to multiply in YUL: https://docs.soliditylang.org/en/latest/yul.html#evm-dialect
+            let z := mul(0x02, x)
+            mstore(0x00, z)
+            return(0x00, 0x20)
+        }
+    }
 }
