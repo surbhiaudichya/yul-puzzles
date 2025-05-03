2850760a9a50f7dca5c9f0845c4a0269120128ff
Surbhi Audichya <audichyasurbhi@gmail.com>
2025-05-03 17:47:10 +0530

    feat: add MaxOfArray yul puzzels solution

--- src/MaxOfArray.sol
+++ src/MaxOfArray.sol
@@ -7,6 +7,20 @@ contract MaxOfArray {
             // your code here
             // return the maximum value in the array
             // revert if array is empty
+            if lt(calldatasize(), 0x24) { revert(0, 0) }
+            let length := calldataload(0x24)
+            if iszero(length) { revert(0, 0) }
+
+            let x := 0x44 // first element
+            let max := calldataload(x)
+
+            for { let i := 1 } lt(i, length) { i := add(i, 1) } {
+                x := add(x, 0x20)
+                let val := calldataload(x)
+                if gt(val, max) { max := val }
+            }
+            mstore(0x00, max)
+            return(0x00, 0x20)
         }
     }
 }
