1ab7bafab6a688c4c197be120b8bbba41f1adb3f
Surbhi Audichya <audichyasurbhi@gmail.com>
2025-05-05 18:00:17 +0530

    feat: add Tenary yul puzzels solution

--- src/Tenary.sol
+++ src/Tenary.sol
@@ -10,6 +10,15 @@ contract Tenary {
             // store `10` in the storage variable `result` if a > b,
             // else if b > c, store `20`,
             // else, store `30`
+            if lt(b, a) {
+                sstore(0x00, 10)
+                return(0x00, 0x00)
+            }
+            if lt(c, b) {
+                sstore(0x00, 20)
+                return(0x00, 0x00)
+            }
+            sstore(0x00, 30)
         }
     }
 }
