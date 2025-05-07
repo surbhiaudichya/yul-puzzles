210d423c08e2e2f02edcbf127c26503229288e1b
Surbhi Audichya <audichyasurbhi@gmail.com>
2025-05-07 17:15:25 +0530

    feat: add WriteToFixedArray yul puzzels solution

--- src/WriteToFixedArray.sol
+++ src/WriteToFixedArray.sol
@@ -9,6 +9,12 @@ contract WriteToFixedArray {
             // your code here
             // store the values in the fixed array `x` in the storage variable `writeHere`
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+            let storageSlot := writeHere.slot
+
+            for { let i := 0 } lt(i, 5) { i := add(i, 1) } {
+                let value := mload(add(x, mul(0x20, i)))
+                sstore(add(storageSlot, i), value)
+            }
         }
     }
 
