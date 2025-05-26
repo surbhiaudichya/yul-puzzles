a5deb0e30351675c92e4f9b471f526dfcf24748d
Surbhi Audichya <audichyasurbhi@gmail.com>
2025-05-26 17:29:15 +0530

    feat: add WriteDynamicArrayToStorage yul puzzels solution

--- src/ReadFromDynamicArrayAndRevertOnFailure.sol
+++ src/ReadFromDynamicArrayAndRevertOnFailure.sol
@@ -15,6 +15,28 @@ contract ReadFromDynamicArrayAndRevertOnFailure {
             // and return it
             // Revert with Solidity panic on failure, use error code 0x32 (out-of-bounds or negative index)
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+            // To-do Fix failed test
+            // load array length
+            let len := sload(readMe.slot)
+
+            // if index < 0 or index >= len, revert with Panic(0x32)
+            if or(lt(index, 0), iszero(lt(index, len))) {
+                // Create proper Solidity panic with selector and error code
+                mstore(0x00, 0x4e487b71) // Panic(uint256) selector
+                mstore(0x04, 0x32) // Error code 0x32 (array out-of-bounds)
+                revert(0x00, 0x24) // 36 bytes total (4 + 32)
+            }
+
+            // calculate base slot for dynamic array data
+            mstore(0x00, readMe.slot)
+            let base := keccak256(0x00, 0x20)
+
+            // load element at index
+            let val := sload(add(base, index))
+
+            // return the value
+            mstore(0x00, val)
+            return(0x00, 0x20)
         }
     }
 }
--- src/WriteDynamicArrayToStorage.sol
+++ src/WriteDynamicArrayToStorage.sol
@@ -8,6 +8,18 @@ contract WriteDynamicArrayToStorage {
         assembly {
             // your code here
             // write the dynamic calldata array `x` to storage variable `writeHere`
+            //[Function Selector (4 bytes)] [Offset to x (32 bytes)] [Length of x (32 bytes)] [x[0] (32 bytes)] [x[1] (32 bytes)] ...
+            let length := calldataload(0x24)
+            // The storage slot allocated for the length of the array is the same slot for the array storage variable (base slot)
+            let baseSlot := writeHere.slot
+            sstore(baseSlot, length)
+            mstore(0x00, baseSlot)
+            let firstElementSlot := keccak256(0x00, 0x20)
+            for { let i := 0 } lt(i, length) { i := add(i, 1) } {
+                let elementSlot := add(firstElementSlot, i)
+                let elementValue := calldataload(add(0x44, mul(i, 0x20)))
+                sstore(elementSlot, elementValue)
+            }
         }
     }
 }
